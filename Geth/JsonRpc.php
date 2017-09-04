e7a3b82a1c995640a95c7e9f6328fa25e57c2652
sdaf47 <sdaf47@yandex.ru>
2017-09-04 11:52:52 +0300

    geth client

---
+++ Geth/JsonRpc.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sdaf47
+ * Date: 04.09.17
+ * Time: 10:50
+ */
+
+namespace Geth;
+
+
+use Exception;
+
+class JsonRpc
+{
+    protected $host, $port, $version;
+    protected $id = 0;
+
+    function __construct($host, $port, $version = "2.0")
+    {
+        $this->host = $host;
+        $this->port = $port;
+        $this->version = $version;
+    }
+
+    function request($method, $params = array())
+    {
+        $data = array();
+        $data['jsonrpc'] = $this->version;
+        $data['id'] = $this->id++;
+        $data['method'] = $method;
+        $data['params'] = $params;
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $this->host);
+        curl_setopt($ch, CURLOPT_PORT, $this->port);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+        $ret = curl_exec($ch);
+
+        if ($ret !== false) {
+            $formatted = $this->format_response($ret);
+
+            if (isset($formatted->error)) {
+                throw new RPCException($formatted->error->message, $formatted->error->code);
+            } else {
+                return $formatted;
+            }
+        } else {
+            throw new RPCException("Server did not respond");
+        }
+    }
+
+    function format_response($response)
+    {
+        return @json_decode($response);
+    }
+}
+
+class RPCException extends Exception
+{
+    public function __construct($message, $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString()
+    {
+        return __CLASS__ . ": " . (($this->code > 0) ? "[{$this->code}]:" : "") . " {$this->message}\n";
+    }
+}
